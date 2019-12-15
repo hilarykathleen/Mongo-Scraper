818ba042ea529a578481c59d06ed4e561eaca7fa
Hilary Lamb <hilaryklamb@gmail.com>
2019-12-15 17:00:32 -0500

    get route console logs title link and summary in node.js

--- routes/scrape.js
+++ routes/scrape.js
@@ -30,3 +30,38 @@ app.get("/scrape", function(req, res) {
       });
     });
 });
+
+
+app.get("/scrape", function(req, res) {
+  axios.get("https://www.nytimes.com/")
+      .then(function(response){
+          // Here's where we store the cheerio data
+          var $ = cheerio.load(response.data);
+          // Look through cheerio data for all the h2's - headlines
+          const foo = $('h2')
+          // Go through each h2 and look for the things we need
+          foo.each(function(i, element){
+              // Empty object that we're eventually going to throw into our database
+              var result = {};
+              // Get the title of the h2
+              result.title = $(this)
+                  .text();
+
+              result.link = $(this)
+                  .children("a")
+                  .attr("href");
+              result.summary = $(this)
+                  .children("ul")
+                  .text();
+                  console.log(result)
+          })
+          res.send('done')
+          // console.log(response, "response")
+          // res.json(response.data)
+      })  
+      .catch(function(error){
+          console.log(error, "error")
+      })
+  // res.send("done")
+
+});
--- server.js
+++ server.js
@@ -31,15 +31,30 @@ mongoose.connect("mongodb://localhost/unit18Populater", { useNewUrlParser: true 
 
 // Here Routes
 
-
 app.get("/scrape", function(req, res) {
-    axios.get("https://www.nytimes.com/")
+    axios.get("https://www.npr.org/sections/news/")
         .then(function(response){
+            // Here's where we store the cheerio data
             var $ = cheerio.load(response.data);
-            const foo = $('h2')
+            // Look through cheerio data for all the h2's - headlines
+            const foo = $('div.item-info')
+            //     console.log(foo)
+            // Go through each h2 and look for the things we need
             foo.each(function(i, element){
+                // Empty object that we're eventually going to throw into our database
                 var result = {};
+                // Get the title of the h2
                 result.title = $(this)
+                    .children("h2.title")
+                    .children("a")
+                    .text();
+                result.link = $(this)
+                    .children("h2.title")
+                    .children("a")
+                    .attr("href");
+                result.summary = $(this)
+                    .children("p")
+                    .last()
                     .text();
                     console.log(result)
             })
@@ -51,16 +66,9 @@ app.get("/scrape", function(req, res) {
             console.log(error, "error")
         })
     // res.send("done")
-
-});
-
-
-
-
-
-
-
-
+  
+  });
+  
 
 
 
