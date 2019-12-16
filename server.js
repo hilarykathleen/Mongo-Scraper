f5240cbfe81fccfc170e29c4e6bcb58eb42afbfe
Hilary Lamb <hilaryklamb@gmail.com>
2019-12-15 20:27:18 -0500

    current

--- server.js
+++ server.js
@@ -56,23 +56,108 @@ app.get("/scrape", function(req, res) {
                     .children("p")
                     .last()
                     .text();
-                    console.log(result)
+                    // console.log(result)
+                result.save = false;
+
+            // pushing results into database
+            db.Article.create(result)
+            .then(function(dbArticle){
+            console.log(dbArticle)
             })
-            res.send('done')
-            // console.log(response, "response")
-            // res.json(response.data)
-        })  
-        .catch(function(error){
-            console.log(error, "error")
-        })
-    // res.send("done")
-  
+            .catch(function(err){
+            return res.json(err)
+      })
+    });
+            res.send("scrape complete")
   });
+})
+
+// Route for getting all Articles from the db
+app.get("/articles", function(req, res) {
+    // Grab every document in the Articles collection
+    db.Article.find({})
+      .then(function(dbArticle) {
+        // If we were able to successfully find Articles, send them back to the client
+        res.json(dbArticle);
+      })
+      .catch(function(err) {
+        // If an error occurred, send it to the client
+        res.json(err);
+      });
+  });
+
+  // Route for grabbing a specific Article by id, populate it with it's note
+  app.get("/articles/:id", function(req, res) {
+    // Using the id passed in the id parameter, prepare a query that finds the matching one in our db...
+    db.Article.findOne({ _id: req.params.id })
+      // ..and populate all of the notes associated with it
+      .populate("note")
+      .then(function(dbArticle) {
+        // If we were able to successfully find an Article with the given id, send it back to the client
+        res.json(dbArticle);
+      })
+      .catch(function(err) {
+        // If an error occurred, send it to the client
+        res.json(err);
+      });
+  });
+
+  // Route for saving/updating an Article's associated Note
+ app.post("/articles/:id", function(req, res) {
+    // Create a new note and pass the req.body to the entry
+    db.Note.create(req.body)
+      .then(function(dbNote) {
+        // If a Note was created successfully, find one Article with an `_id` equal to `req.params.id`. Update the Article to be associated with the new Note
+        // { new: true } tells the query that we want it to return the updated User -- it returns the original by default
+        // Since our mongoose query returns a promise, we can chain another `.then` which receives the result of the query
+        return db.Article.findOneAndUpdate({ _id: req.params.id }, { note: dbNote._id }, { new: true });
+      })
+      .then(function(dbArticle) {
+        // If we were able to successfully update an Article, send it back to the client
+        res.json(dbArticle);
+      })
+      .catch(function(err) {
+        // If an error occurred, send it to the client
+        res.json(err);
+      });
+  });
+
+  app.post("/savethisarticle/:id", function(req,res){
+    db.Article.findOneAndUpdate({_id: req.params.id}, {artsave:true})
+    .then(function(dbNote){
+      res.json(dbNote)
+    })
+    .catch(function(error){
+      res.json(error)
+    })
+  })
   
-
-
-
-
+  app.delete("/deletethisnote/:id", function(req,res){
+    db.Note.deleteOne({_id: req.params.id})
+    .then(function(){
+      res.send()
+    })
+    .catch(function(error){
+      res.json(error)
+    })
+  })
+  
+  //deletes article and all notes associated with it
+  app.delete("/deletethisarticle/:id", function(req,res){
+    db.Note.deleteMany({article: req.params.id})
+    .then(function(){
+      db.Article.deleteOne({_id:req.params.id})
+      .then(function(){
+        res.send();
+      })
+      .catch(function(err){
+        console.log(err);
+      })
+    })
+    .catch(function(error){
+      res.json(error)
+    })
+  })
 
 app.listen(PORT, function() {
     console.log("App running on port " + PORT + "!");
